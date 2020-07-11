4d0ff938329bbaf734a581dab9f98984b203946b
Annie Huang <annie.hn@gmail.com>
2020-07-11 15:50:30 +1000

    Pass the value user enter in input from Search.js to parent component App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,11 @@ import Search from "./components/ui/Search";
 const App = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [query, setQuery] = useState('');
 
     useEffect(() => {
         const fetchItems = async () => {
-            const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+            const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
 
             console.log(result.data);
             setItems(result.data);
@@ -24,7 +25,7 @@ const App = () => {
     return (
         <div className="container">
             <Header />
-            <Search />
+            <Search getQuery={() => {}} />
             <CharacterGrid isLoading={isLoading} items={items} />
         </div>
     );
